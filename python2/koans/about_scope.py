1a1032b65ec0d64f5e360fd8c33031b3c8ce46b0
Karambir Singh Nain <akarambir@gmail.com>
2014-06-18 19:52:28 +0530

    done 289 koans and 36 lessons

--- python2/koans/about_scope.py
+++ python2/koans/about_scope.py
@@ -89,4 +89,4 @@ class AboutScope(Koan):
     deadly_bingo = [4, 8, 15, 16, 23, 42]
 
     def test_global_attributes_can_be_created_in_the_middle_of_a_class(self):
-        self.assertEqual(__, deadly_bingo[5])
+        self.assertEqual(42, deadly_bingo[5])
